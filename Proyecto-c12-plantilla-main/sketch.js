fcea88973f00f20ec65139c2506aad4eb75f803c
LULUCITAES <LOURDES.ESPINODELCASTILLO@GMAIL.COM>
2022-03-14 00:03:07 -0600

    EL ERROR ES LA Ñ QUE DEJASTE AL PRINICIO!
    checa los comentarios que te puse :)

--- Proyecto-c12-plantilla-main/sketch.js
+++ Proyecto-c12-plantilla-main/sketch.js
@@ -1,15 +1,15 @@
-var nino, niñoImg;
+var nino, niñoImg; // AQUI ESTA EL ERROR! CAMBIA LA Ñ 
 
 
 function preload(){
- ninoImg=loadAnimation("jake3.png","jake5.png");
+ ninoImg=loadAnimation("jake3.png","jake5.png"); // AQUI PUEDES AGREGAR TODAS LAS IMAGENES, JUSTO COMO LO HICISTE ESCRIBIENDO CADA UNA COMO ES 
  
 }
 
 function setup(){
   createCanvas(400,400);
 nino=createSprite(200,200,20,50);  
-nino.addAnimation("c",niñoImg);
+nino.addAnimation("c",niñoImg); // AQUI TAMBIEN HAY QUE CAMBIAR LA Ñ 
 
 }
 
